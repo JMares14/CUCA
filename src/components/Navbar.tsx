af20b5f839d8151b1a1cd0420714e9953107381a
JM <georgemares14@yahoo.com>
2025-05-25 13:23:01 -0700

    Working on footer header and navbar.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import Menu from "./Menu"
+
+const Navbar = () => {
+    return (
+        <div className='h-20 px4-4 md:px-8 lg:px-16 xl:32 2xl:px-64'>
+            {/* MOBILE */}
+            <Link href="/">CUCA</Link>
+            <Menu/>
+        </div>
+    )
+}
+export default Navbar
